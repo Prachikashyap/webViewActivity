8d1e2a237f4e05d881e446e817835b153574c6b3
prachi <prachikashyap022@gmail.com>
2018-02-16 12:38:08 +0530

    Second commit

--- app/src/main/java/com/example/prachi/webviewactivity/WebAppInterface.java
+++ app/src/main/java/com/example/prachi/webviewactivity/WebAppInterface.java
@@ -34,25 +34,7 @@ public class WebAppInterface {
 
 
     // Show a toast from the web page
-    @JavascriptInterface
-    public void showToast(String toast) {
-        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
-    }
 
-    @JavascriptInterface
-    public int getAndroidVersion() {
-        return android.os.Build.VERSION.SDK_INT;
-    }
-
-    @JavascriptInterface
-    public void showAndroidVersion(String versionName) {
-        Toast.makeText(mContext, versionName, Toast.LENGTH_SHORT).show();
-    }
-
-    @JavascriptInterface
-    public void calcSomething(int c) {
-        Toast.makeText(mContext, "value after sum" + c, Toast.LENGTH_SHORT).show();
-    }
 
     @JavascriptInterface
     public void saveValues(String usr, String pwd) {
